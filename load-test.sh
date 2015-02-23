ab71128481b105dd850855add57d26fcae16fb91
Ben Green <ben@latenightsketches.com>
2015-02-23 00:41:32 -0800

    Load test now only runs scoresLoad

--- load-test.sh
+++ load-test.sh
@@ -5,6 +5,6 @@ class_counts=( 100 )
 for count in "${class_counts[@]}"
 do
   echo "Running test with class count: $count"
-  CONN="postgres://meteor:meteor@127.0.0.1/meteor_test" CHANNEL="scores_load" DEBUG=0 STATS=1 CLASS_COUNT=$count node_modules/nodeunit/bin/nodeunit test/index.js >> oot.log
+  CONN="postgres://meteor:meteor@127.0.0.1/meteor_test" CHANNEL="scores_load" LOAD_TEST=1 DEBUG=0 STATS=1 CLASS_COUNT=$count node_modules/nodeunit/bin/nodeunit test/index.js >> oot.log
 done
 
--- test/index.js
+++ test/index.js
@@ -9,8 +9,9 @@ if(!('CHANNEL' in process.env))
 		'CHANNEL environment variable required! (notification identifier string)');
 
 // Global flags
-global.printDebug = process.env.DEBUG !== undefined && process.env.DEBUG !== '0';
-global.printStats = process.env.STATS !== undefined && process.env.STATS !== '0';
+global.printDebug = process.env.DEBUG === '1';
+global.printStats = process.env.STATS === '1';
+global.isLoadTest = process.env.LOAD_TEST === '1';
 
 // ES6 may be used in all files required by this one
 require('babel/register');
@@ -21,9 +22,17 @@ var PgTriggers = require('../');
 // Define global instance
 global.triggers   = new PgTriggers(process.env.CONN, process.env.CHANNEL);
 
-module.exports = _.assign(
-	require('./helpers/lifecycle'),
-	// Load each test module
-	require('./scoresLoad'), // Optional CLASS_COUNT env variable, default 1
-	require('./variousQueries')
-);
+if(isLoadTest) {
+	module.exports = _.assign(
+		require('./helpers/lifecycle'),
+		require('./scoresLoad')
+	);
+}
+else{
+	// Load full test suite
+	module.exports = _.assign(
+		require('./helpers/lifecycle'),
+		require('./scoresLoad'), // Optional CLASS_COUNT env variable, default 1
+		require('./variousQueries')
+	);
+}
